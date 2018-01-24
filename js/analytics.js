f83298abca5a8106b173065e81b7c8c573cd774c
tangjiao <1296724559@qq.com>
2018-01-24 17:56:53 +0800

    数据统计

---
+++ js/analytics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?8fe849b42fe1006b86216f81784f8567";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
